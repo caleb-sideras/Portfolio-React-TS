15e309daf3e4d60cf8a58cbd6a9e318730bd0839
Caleb Sideras <caleb.sideras@outlook.com>
2023-03-28 23:16:11 -0400

    Added Tweetailyze in projects

--- src/Routes/Projects.tsx
+++ src/Routes/Projects.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react'
 import { AnimationOnScroll } from 'react-animation-on-scroll'
 import { FaVuejs } from 'react-icons/fa'
-import { SiAmazonaws, SiCplusplus, SiDart, SiDjango, SiDotnet, SiFlutter, SiMicrosoftbing, SiMysql, SiOpenai, SiPython, SiReact, SiTypescript, SiWebpack } from 'react-icons/si'
+import { SiAmazonaws, SiCelery, SiCplusplus, SiDart, SiDjango, SiDocker, SiDotnet, SiFastapi, SiFlutter, SiMicrosoftbing, SiMysql, SiOpenai, SiPostgresql, SiPython, SiPytorch, SiReact, SiRedis, SiTypescript, SiWebpack } from 'react-icons/si'
 import { CgArrowTopRight } from 'react-icons/cg'
 
 type Props = {}
@@ -10,6 +10,42 @@ const Projects: React.FC = () => {
     return (
 
         <div className='py-8 flex flex-col gap-8'>
+            <div className='max-w-xl w-full justify-center items-center flex flex-col animate-fade-in px-4 border-4'>
+                <a href="https://github.com/caleb-sideras/TweetailyzeV1" target='_blank'>
+                    <div className='flex flex-row items-center ml-2 hover:text-accent transition-all'>
+                        <span className='font-bold text-3xl my-8'>Tweetailyze</span>
+                        <CgArrowTopRight />
+                    </div>
+                </a>
+                <span className='text-xl font-light text-center w-full pb-4 text-adjust-center '>An API that provides Twitter account summarization through embedding/clustering of tweets, and then performing sentiment analysis and topic modeling.</span>
+                <span className='text-xl font-bold text-center w-full py-4'>Tech Stack</span>
+                    <div className='flex flex-row flex-wrap items-center gap-2'> 
+                        <SiDocker className='button-icon-left' />
+                        +
+                        <SiFastapi className='button-icon-left' />
+                        +
+                        <SiPostgresql className='button-icon-left' />
+                        +
+                        <SiCelery className='button-icon-left' />
+                        +
+                        <SiRedis className='button-icon-left' />
+                    </div>
+                    <div className='flex flex-row flex-wrap items-center gap-2 mt-4'>
+                        <SiPytorch className='button-icon-left' />
+                        +
+                        <SiOpenai className='button-icon-left' />
+                    </div>
+                <span className='text-xl font-light text-left w-full py-4 gap-2 flex flex-wrap justify-center'>
+                    Other:
+                    <a href="https://github.com/cjhutto/vaderSentiment" className='underline' target='_blank'>VADER-Sentiment-Analysis</a>
+                    <a href="https://huggingface.co/bert-base-multilingual-cased" className='underline' target='_blank'>BBERT multilingual base model</a>
+                    <a href="https://www.nltk.org/" className='underline' target='_blank'>Natural Language Toolkit</a>
+                    <a href="https://scikit-learn.org/stable/modules/generated/sklearn.decomposition.NMF.html" className='underline' target='_blank'>Non-Negative Matrix Factorization</a>
+                    <a href="https://www.pgadmin.org/" className='underline' target='_blank'>pgAdmin</a>
+                    <a href="https://flower.readthedocs.io/en/latest/" className='underline' target='_blank'>Flower</a>
+                    <a href="https://www.sqlalchemy.org/" className='underline' target='_blank'>SQLAlchemy</a>
+                </span>
+            </div>
             <div className='max-w-xl w-full justify-center items-center flex flex-col animate-fade-in px-4 border-4'>
                 <a href="https://aipeoplepedia.com/" target='_blank'>
                     <div className='flex flex-row items-center ml-2 hover:text-accent transition-all'>
@@ -24,16 +60,16 @@ const Projects: React.FC = () => {
                         +
                         <SiTypescript className='button-icon-left' />
                     </div>
-                    {/* <div className='flex flex-row flex-wrap items-center gap-2'>
+                    <div className='flex flex-row flex-wrap items-center gap-2 mt-2'>
                         <SiOpenai className='button-icon-left' />
                         +
                         <SiMicrosoftbing className='button-icon-left' />
-                    </div> */}
+                    </div>
                     <div className='flex flex-row flex-wrap items-center gap-2 mt-4'>
                         <SiDjango className='button-icon-left' />
                     </div>
-                <span className='text-xl font-light text-left w-full py-4 gap-2 flex flex-wrap'>
-                    Technologies/APIs:
+                <span className='text-xl font-light text-left w-full py-4 gap-2 flex flex-wrap justify-center'>
+                    Other:
                     <a href="https://platform.openai.com/docs/api-reference/completions" className='underline' target='_blank'>OpenAI GPT-3.5</a>
                     <a href="https://www.microsoft.com/en-us/bing/apis/bing-web-search-api" className='underline' target='_blank'>Bing Web Search</a>
                 </span>
@@ -52,8 +88,8 @@ const Projects: React.FC = () => {
                         +
                         <SiDart className='button-icon-left' />
                     </div>
-                <span className='text-xl font-light text-left w-full py-4 gap-2 flex flex-wrap'>
-                    Technologies/APIs:
+                <span className='text-xl font-light text-left w-full py-4 gap-2 flex flex-wrap justify-center'>
+                    Other:
                     <a href="https://www.coingecko.com/en/api" className='underline' target='_blank'>CoinGecko</a>
                     <a href="https://github.com/binance/binance-spot-api-docs/blob/master/rest-api.md" className='underline' target='_blank'>Binance</a>
                 </span>
@@ -71,8 +107,8 @@ const Projects: React.FC = () => {
                 <div className='flex flex-row flex-wrap items-center gap-2'>
                     <SiPython className='button-icon-left' />
                 </div>
-                <span className='text-xl font-light text-left w-full py-4 gap-2 flex flex-wrap'>
-                    Technologies/APIs:
+                <span className='text-xl font-light text-left w-full py-4 gap-2 flex flex-wrap justify-center'>
+                    Other:
                     <a href="https://docs.ftx.com/#overview" className='underline' target='_blank'>FTX</a>
                     <a href="https://github.com/binance/binance-spot-api-docs/blob/master/rest-api.md" className='underline' target='_blank'>Binance</a>
                     <a href="https://docs.cloud.coinbase.com/exchange/docs/welcome" className='underline' target='_blank'>Coinbase Pro</a>
@@ -93,8 +129,8 @@ const Projects: React.FC = () => {
                 <div className='flex flex-row flex-wrap items-center gap-2'>
                     <SiCplusplus className='button-icon-left' />
                 </div>
-                <span className='text-xl font-light text-left w-full py-4 gap-2 flex flex-wrap'>
-                    Technologies/APIs:
+                <span className='text-xl font-light text-left w-full py-4 gap-2 flex flex-wrap justify-center'>
+                    Other:
                     <a href="https://docs.microsoft.com/en-us/windows/win32/winsock/windows-sockets-start-page-2" className='underline' target='_blank'>WinSock API</a>
                 </span>
             </div>
@@ -111,8 +147,8 @@ const Projects: React.FC = () => {
                 <div className='flex flex-row flex-wrap items-center gap-2'>
                     <SiDotnet className='button-icon-left'/>
                 </div>
-                <span className='text-xl font-light text-left w-full py-4 gap-2 flex flex-wrap'>
-                    Technologies/APIs:
+                <span className='text-xl font-light text-left w-full py-4 gap-2 flex flex-wrap justify-center'>
+                    Other:
                     <a href="https://docs.microsoft.com/en-us/dotnet/" className='underline' target='_blank'>.NET</a>
                 </span>
             </div>
