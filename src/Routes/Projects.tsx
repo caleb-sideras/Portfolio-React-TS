777b2b1c7b70a01f4170f48e04410692ae595bcc
Caleb Sideras <caleb.sideras@outlook.com>
2023-05-18 16:29:10 -0400

    'Added MusicGPT'

--- src/Routes/Projects.tsx
+++ src/Routes/Projects.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react'
 import { AnimationOnScroll } from 'react-animation-on-scroll'
 import { FaVuejs } from 'react-icons/fa'
-import { SiAmazonaws, SiCelery, SiCplusplus, SiDart, SiDjango, SiDocker, SiDotnet, SiFastapi, SiFlutter, SiMicrosoftbing, SiMysql, SiOpenai, SiPostgresql, SiPython, SiPytorch, SiReact, SiRedis, SiTypescript, SiWebpack } from 'react-icons/si'
+import { SiAmazonaws, SiCelery, SiCplusplus, SiDart, SiDjango, SiDocker, SiDotnet, SiFastapi, SiFlutter, SiMicrosoftbing, SiMysql, SiNextdotjs, SiOpenai, SiPostgresql, SiPython, SiPytorch, SiReact, SiRedis, SiTypescript, SiVercel, SiWebpack } from 'react-icons/si'
 import { CgArrowTopRight } from 'react-icons/cg'
 
 type Props = {}
@@ -11,6 +11,30 @@ const Projects: React.FC = () => {
 
         <div className='py-8 flex flex-col gap-8'>
             <div className='max-w-xl w-full justify-center items-center flex flex-col animate-fade-in px-4 border-4'>
+                <a href="https://music-gpt.vercel.app/" target='_blank'>
+                    <div className='flex flex-row items-center ml-2 hover:text-accent transition-all'>
+                        <span className='font-bold text-3xl my-8'>MusicGPT</span>
+                        <CgArrowTopRight />
+                    </div>
+                </a>
+                <span className='text-xl font-light text-center w-full pb-4 text-adjust-center '>Discuss various technical aspects about a song w/ MusicGPT Pro & Lite.</span>
+                <span className='text-xl font-bold text-center w-full py-4'>Tech Stack</span>
+                <div className='flex flex-row flex-wrap items-center gap-2'>
+                    <SiVercel className='button-icon-left' />
+                    +
+                    <SiNextdotjs className='button-icon-left' />
+                </div>
+                <div className='flex flex-row flex-wrap items-center gap-2 mt-4'>
+                    <SiOpenai className='button-icon-left' />
+                </div>
+                <span className='text-xl font-light text-left w-full py-4 gap-2 flex flex-wrap justify-center'>
+                    Other:
+                    <a href="https://basicpitch.spotify.com/" className='underline' target='_blank'>BasicPitch</a>
+                    <a href="https://mtg.github.io/essentia.js/" className='underline' target='_blank'>Essentia</a>
+                    <a href="https://webassembly.org/" className='underline' target='_blank'>Web Assembly</a>
+                </span>
+            </div>
+            <div className='max-w-xl w-full justify-center items-center flex flex-col animate-fade-in px-4 border-4'>
                 <a href="https://tweetailyze-frontend.vercel.app/" target='_blank'>
                     <div className='flex flex-row items-center ml-2 hover:text-accent transition-all'>
                         <span className='font-bold text-3xl my-8'>Tweetailyze</span>
@@ -19,20 +43,20 @@ const Projects: React.FC = () => {
                 </a>
                 <span className='text-xl font-light text-center w-full pb-4 text-adjust-center '>A web app that performs Twitter account summarization through tweet embedding, clustering, sentiment analysis and topic modeling.</span>
                 <span className='text-xl font-bold text-center w-full py-4'>Tech Stack</span>
-                    <div className='flex flex-row flex-wrap items-center gap-2'> 
-                        <SiDocker className='button-icon-left' />
-                        +
-                        <SiFastapi className='button-icon-left' />
-                        +
-                        <SiPostgresql className='button-icon-left' />
-                        +
-                        <SiCelery className='button-icon-left' />
-                        +
-                        <SiRedis className='button-icon-left' />
-                    </div>
-                    <div className='flex flex-row flex-wrap items-center gap-2 mt-4'>
-                        <SiOpenai className='button-icon-left' />
-                    </div>
+                <div className='flex flex-row flex-wrap items-center gap-2'>
+                    <SiDocker className='button-icon-left' />
+                    +
+                    <SiFastapi className='button-icon-left' />
+                    +
+                    <SiPostgresql className='button-icon-left' />
+                    +
+                    <SiCelery className='button-icon-left' />
+                    +
+                    <SiRedis className='button-icon-left' />
+                </div>
+                <div className='flex flex-row flex-wrap items-center gap-2 mt-4'>
+                    <SiOpenai className='button-icon-left' />
+                </div>
                 <span className='text-xl font-light text-left w-full py-4 gap-2 flex flex-wrap justify-center'>
                     Other:
                     <a href="https://github.com/cjhutto/vaderSentiment" className='underline' target='_blank'>VADER-Sentiment-Analysis</a>
@@ -52,19 +76,19 @@ const Projects: React.FC = () => {
                 </a>
                 <span className='text-xl font-light text-center w-full pb-4 text-adjust-center '>AI powered search that summarizes and visualizes information for anyone who has data on the internet.</span>
                 <span className='text-xl font-bold text-center w-full py-4'>Tech Stack</span>
-                    <div className='flex flex-row flex-wrap items-center gap-2'>
-                        <SiReact className='button-icon-left' />
-                        +
-                        <SiTypescript className='button-icon-left' />
-                    </div>
-                    <div className='flex flex-row flex-wrap items-center gap-2 mt-2'>
-                        <SiOpenai className='button-icon-left' />
-                        +
-                        <SiMicrosoftbing className='button-icon-left' />
-                    </div>
-                    <div className='flex flex-row flex-wrap items-center gap-2 mt-4'>
-                        <SiDjango className='button-icon-left' />
-                    </div>
+                <div className='flex flex-row flex-wrap items-center gap-2'>
+                    <SiReact className='button-icon-left' />
+                    +
+                    <SiTypescript className='button-icon-left' />
+                </div>
+                <div className='flex flex-row flex-wrap items-center gap-2 mt-2'>
+                    <SiOpenai className='button-icon-left' />
+                    +
+                    <SiMicrosoftbing className='button-icon-left' />
+                </div>
+                <div className='flex flex-row flex-wrap items-center gap-2 mt-4'>
+                    <SiDjango className='button-icon-left' />
+                </div>
                 <span className='text-xl font-light text-left w-full py-4 gap-2 flex flex-wrap justify-center'>
                     Other:
                     <a href="https://platform.openai.com/docs/api-reference/completions" className='underline' target='_blank'>OpenAI GPT-3.5</a>
@@ -80,11 +104,11 @@ const Projects: React.FC = () => {
                 </a>
                 <span className='text-xl font-light text-center w-full pb-4 text-adjust-center '>Flutter app that allows for personalization and real-time updates for cryptocurrency tracking. Uses Flutter BLoC architecture.</span>
                 <span className='text-xl font-bold text-center w-full py-4'>Tech Stack</span>
-                    <div className='flex flex-row flex-wrap items-center gap-2'>
-                        <SiFlutter className='button-icon-left' />
-                        +
-                        <SiDart className='button-icon-left' />
-                    </div>
+                <div className='flex flex-row flex-wrap items-center gap-2'>
+                    <SiFlutter className='button-icon-left' />
+                    +
+                    <SiDart className='button-icon-left' />
+                </div>
                 <span className='text-xl font-light text-left w-full py-4 gap-2 flex flex-wrap justify-center'>
                     Other:
                     <a href="https://www.coingecko.com/en/api" className='underline' target='_blank'>CoinGecko</a>
@@ -110,7 +134,7 @@ const Projects: React.FC = () => {
                     <a href="https://github.com/binance/binance-spot-api-docs/blob/master/rest-api.md" className='underline' target='_blank'>Binance</a>
                     <a href="https://docs.cloud.coinbase.com/exchange/docs/welcome" className='underline' target='_blank'>Coinbase Pro</a>
                     <a href="https://phemex.com/user-guides/api-overview" className='underline' target='_blank'>Phemex</a>
-                    
+
                 </span>
             </div>
             <div className='max-w-xl w-full justify-center items-center flex flex-col animate-fade-in px-4 border-4'>
@@ -142,7 +166,7 @@ const Projects: React.FC = () => {
                 <span className='text-xl font-light text-center w-full pb-4 text-adjust-center'>The objective of the game is to survive while defeating all opponents. Enemy and level information is data driven via xml files. Once all levels are completed the game will restart at level zero.</span>
                 <span className='text-xl font-bold text-center w-full py-4'>Tech Stack</span>
                 <div className='flex flex-row flex-wrap items-center gap-2'>
-                    <SiDotnet className='button-icon-left'/>
+                    <SiDotnet className='button-icon-left' />
                 </div>
                 <span className='text-xl font-light text-left w-full py-4 gap-2 flex flex-wrap justify-center'>
                     Other:
